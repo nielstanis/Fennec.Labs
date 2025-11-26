2af85e0bb693a834338bc145fa6e6422a879663c
Niels Tanis <nielstanis@live.nl>
2025-11-26 21:46:43 +0100

    TestProjects

--- test/FennecLabs.Instrumentation.Tests/AssemblyAnalyzerTests.cs
+++ test/FennecLabs.Instrumentation.Tests/AssemblyAnalyzerTests.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using FennecLabs.Instrumentation;
 using FennecLabs.Instrumentation.Result;
+using FennecLabs.TestUtilities;
 using Xunit;
 
 namespace FennecLabs.Instrumentation.Tests
